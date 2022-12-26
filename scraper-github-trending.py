ccd715583a999fe2b29fa090087e659e235e1de6
kobyrthr-dev <kobyrthr@gmail.com>
2022-12-26 15:44:43 -0500

    cleanup

--- scraper-github-trending.py
+++ scraper-github-trending.py
@@ -10,18 +10,17 @@ from selenium.webdriver.common.by import By
 import csv
 
 driver = webdriver.Chrome(executable_path ='/usr/lib/chromium-browser/chromedriver')
-
 driver.get('https://clutch.co/agencies/digital-design?geona_id=26303&related_services=field_pp_sl_web_programming&related_services=field_pp_sl_app_interface_design&related_services=field_pp_sl_digital_strategy&related_services=field_pp_sl_web_design')
 driver.implicitly_wait(100)
-driver.maximize_window()
-r=1
-templist = []
-list_div = driver.find_elements(By.CLASS_NAME, 'company_title')
-
-while(r<3):
-    for list_item in list_div:
-        print(list_item.text)
+scraped_companies = []
+company_titles = driver.find_elements(By.CLASS_NAME, 'company_title')
+r=0
+while r<100:
+    for title in company_titles:
+        scraped_companies.append(title.text)
+        print(title.text)
+        r+=1
 		
-# saving the dataframe to a csv
-# df.to_csv('table.csv')
+df = pd.DataFrame(scraped_companies)
+df.to_csv('table.csv')
 driver.close()
